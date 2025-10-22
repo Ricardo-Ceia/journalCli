0b42719852652812e57acbebb37b78607136f14c
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-10-22 23:04:40 +0100

    add init and close function to the db package

--- TODO.md
+++ TODO.md
@@ -1,3 +1,3 @@
 #TODO: Check why when the user is created, the user's properties are not being set correctly.
-#TODO : Get the logging system working properly.
+#Connect an external database so the server and the client can have a shared memory access.
 #TODO : Refactor the code.
--- db/db.go
+++ db/db.go
@@ -1,9 +1,25 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	"journalCli/handlers"
+	"log"
 )
+
+func InitDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "database=journal.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func CloseDB(db *sql.DB) {
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func GetUserByID(id string) (handlers.User, error) {
 	for _, user := range handlers.Users {
