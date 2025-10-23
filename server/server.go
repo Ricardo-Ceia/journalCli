55b413b83aaa7c5cd9307adc76dc348693a6e669
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-10-23 15:41:08 +0100

    connecting the db to the current code structure and set the initial schema

--- server/server.go
+++ server/server.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"journalCli/db"
 	"journalCli/handlers"
 	"net/http"
 )
 
 func server() {
+	dbPath := "../journal.db"
+
+	database := db.InitDB(dbPath)
+
+	defer db.CloseDB(database)
+
 	http.HandleFunc("/signup", handlers.SignUpHandler)
 	http.HandleFunc("/login", handlers.LoginHandler)
 	fmt.Println("Server running on http://localhost:8080")
